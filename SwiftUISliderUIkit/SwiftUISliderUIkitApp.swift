65c163c1b8ce0b8dad3422043ea02aac031f7880
Konstantin <diel404@gmail.com>
2024-03-17 23:56:36 +0300

    Initial Commit

---
+++ SwiftUISliderUIkit/SwiftUISliderUIkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderUIkitApp.swift
+//  SwiftUISliderUIkit
+//
+//  Created by horze on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderUIkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
